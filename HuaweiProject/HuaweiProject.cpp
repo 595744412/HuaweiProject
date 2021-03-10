67222782078fa5ae28c0b87b62c9c06d4e1d5144
595744412 <595744412@qq.com>
2021-03-10 21:26:29 +0800

    v1.01

--- HuaweiProject/HuaweiProject.cpp
+++ HuaweiProject/HuaweiProject.cpp
@@ -1,9 +1,6 @@
-﻿// HuaweiProject.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
-
+﻿#include <iostream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
